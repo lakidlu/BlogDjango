4f19e20779934fea811bd6f04b1a8fc8a653c3b6
Lukasz Szyndrowski <lukasz.szyndrowski@gmail.com>
2023-05-26 11:25:14 +0200

    dodanie wszystkiego

---
+++ mysite/blog/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from . import views
+
+app_name = 'blog'
+
+urlpatterns = [
+    # Post views
+    path('', views.post_list, name='post list'),
+    #path('', views.PostListView.as_view(), name='post_list'),
+    path('<int:year>/<int:month>/<int:day>/<slug:post>/',
+         views.post_details,
+         name='post_details'),
+    path('<int:post_id>/share/',
+         views.post_share, name='post_share'),
+    path('tag/<slug:tag_slug>/',
+         views.post_list, name='post_list_by_tag'),
+]
